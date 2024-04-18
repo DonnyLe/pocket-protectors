77f078887aa03b8b98e28e8df85d5e0c888d45f1
Jasmine McCoy <mccoy.jas@northeastern.edu>
2024-04-18 23:59:49 +0000

    groups

--- pages/Groups/jsobjects/JSObject2/JSObject2.js
+++ pages/Groups/jsobjects/JSObject2/JSObject2.js
@@ -1,12 +1,11 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
+	Button1onClick () {
+		const admin = all_users.data.find(user => user.email === Input1.text);
+		
+		// update the stored value for user.group_id
+		 storeValue("user.group_id", admin.group_id)
+		
+		// If user is found, return the user object, otherwise return null
+		return admin.group_id || null;
 	}
 }
--- pages/Log_In/jsobjects/JSObject1/JSObject1.js
+++ pages/Log_In/jsobjects/JSObject1/JSObject1.js
@@ -2,7 +2,18 @@ export default {
 	Button1onClick () {
 		const user = all_users.data.find(user => user.email === Email.text && user.password === Password.text);
 		
-		storeValue("user_id", user.id, true);
+		
+	//store user data
+	storeValue("user", {
+											"user_id": user.user_id , 
+											 "email": user.email,
+											"first_name": user.first_name,
+											"group_id": user.group_id,
+											"last_name": user.last_name,
+											"middle_name": user.middle_name,
+											"password": user.password,
+										 
+										 }) 
 		
 		// If user is found, return the user object, otherwise return null
 		return user || null;
