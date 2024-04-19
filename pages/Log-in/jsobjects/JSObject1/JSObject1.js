66db5e1c22283e920f6a717dec38b48dd5fc9f39
Donny Le <le.don@northeastern.edu>
2024-04-19 03:48:55 +0000

    added stuff

--- pages/Log-in/jsobjects/JSObject1/JSObject1.js
+++ pages/Log-in/jsobjects/JSObject1/JSObject1.js
@@ -1,21 +1,21 @@
 export default {
 	Button1onClick () {
 		const user = all_users.data.find(user => user.email === Email.text && user.password === Password.text);
-		
-		
-	//store user data
-	storeValue("user", {
-											"user_id": user.user_id , 
-											 "email": user.email,
-											"first_name": user.first_name,
-											"group_id": user.group_id,
-											"last_name": user.last_name,
-											"middle_name": user.middle_name,
-											"password": user.password
-										 })
+		storeValue("user_id", user.user_id, true)
+
+		//store user data
+		storeValue("user", {
+			"user": user.user_id,
+			"email": user.email,
+			"first_name": user.first_name,
+			"group_id": user.group_id,
+			"last_name": user.last_name,
+			"middle_name": user.middle_name,
+			"password": user.password
+		})
 		//store all user data
 		storeValue("users", all_users.data)
-		
+
 		// If user is found, return the user object, otherwise return null
 		return user || null;
 	}
