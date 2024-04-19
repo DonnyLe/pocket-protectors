21c7baad8be8e8cc3080936920dc8556cde5806f
Tisya Sharma <sharma.ti@northeastern.edu>
2024-04-19 00:11:42 +0000

    t

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -1,5 +1,6 @@
 export default {
 	Button1onClick () {
+		
 		 const user = Get_Users.data.find(user => user.email === Email.text && user.password === Password.text);
      storeValue("user_id", user.user_id, true) 
 		// // If user is found, return the user object, otherwise return null
