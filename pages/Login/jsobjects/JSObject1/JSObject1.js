bf1f570550c17899849f961dc1c15347e7e11450
Tisya Sharma <sharma.ti@northeastern.edu>
2024-04-19 00:15:22 +0000

    h

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -4,6 +4,5 @@ export default {
 		 const user = Get_Users.data.find(user => user.email === Email.text && user.password === Password.text);
      storeValue("user_id", user.user_id, true) 
 		// // If user is found, return the user object, otherwise return null
-	  return user || null;
 	}
 }
